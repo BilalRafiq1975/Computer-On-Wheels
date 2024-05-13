8492feace2584e12dd012ea3678eee18ee69b6b5
Hamza Azhar <140899654+HamzaAzhar-ux@users.noreply.github.com>
2024-05-13 14:46:16 +0500

    Delete unnecessary files

--- build/drivehub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maanz-ai/ahmed-workspace/src/drivehub
-# Build directory: /home/maanz-ai/ahmed-workspace/build/drivehub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
