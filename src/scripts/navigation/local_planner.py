d4a476d73fce036ae2d4f40136de6564dab38c60
Bilal Rafiq <156061139+BilalRafiq1975@users.noreply.github.com>
2024-06-04 17:27:59 +0500

    Update local_planner.py

--- src/scripts/navigation/local_planner.py
+++ src/scripts/navigation/local_planner.py
@@ -1,8 +1,3 @@
-# Copyright (c) # Copyright (c) 2018-2020 CVC.
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
 """ This module contains a local planner to perform low-level waypoint following based on PID controllers. """
 
 from enum import Enum
