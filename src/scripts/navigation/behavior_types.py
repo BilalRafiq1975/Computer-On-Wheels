b50eef2025a2f3c1c825ca853c7c49c83ec55dfd
Bilal Rafiq <156061139+BilalRafiq1975@users.noreply.github.com>
2024-06-04 17:19:37 +0500

    Update behavior_types.py

--- src/scripts/navigation/behavior_types.py
+++ src/scripts/navigation/behavior_types.py
@@ -1,5 +1,4 @@
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
+
 
 """ This module contains the different parameters sets for each behavior. """
 
