9e9927e7500d1c29d719b84d7612ff321b09452c
Mohsin Saghir <157951173+28016mohsin@users.noreply.github.com>
2024-10-13 14:08:59 +0500

    Update behavioragent.py

--- src/scripts/Obstacle Detection/behavioragent.py
+++ src/scripts/Obstacle Detection/behavioragent.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import Float32, Bool
-from carla_msgs.msg import CarlaEgoVehicleControl, CarlaTrafficLightInfo
+""" This module implements an agent that roams around a track following random
+waypoints and avoiding other vehicles. The agent also responds to traffic lights,
+traffic signs, and has different possible configurations. """
 
-# Other existing imports
 import random
 import numpy as np
 import carla
+import rospy  # Import ROS library
 from agents.navigation.basic_agent import BasicAgent
 from agents.navigation.local_planner import RoadOption
 from agents.navigation.behavior_types import Cautious, Aggressive, Normal
@@ -17,18 +16,27 @@ class BehaviorAgent(BasicAgent):
     BehaviorAgent implements an agent that navigates scenes to reach a given
     target destination, by computing the shortest possible path to it.
     This agent can correctly follow traffic signs, speed limitations,
-    traffic lights, while also taking into account nearby vehicles.
+    traffic lights, while also taking into account nearby vehicles. Lane changing
+    decisions can be taken by analyzing the surrounding environment such as tailgating avoidance.
+    Adding to these are possible behaviors, the agent can also keep safety distance
+    from a car in front of it by tracking the instantaneous time to collision
+    and keeping it in a certain range. Finally, different sets of behaviors
+    are encoded in the agent, from cautious to a more aggressive ones.
     """
 
     def __init__(self, vehicle, behavior='normal'):
         """
         Constructor method.
 
-        :param vehicle: actor to apply local planner logic to
-        :param behavior: type of agent to apply
+            :param vehicle: actor to apply to local planner logic onto
+            :param ignore_traffic_light: boolean to ignore any traffic light
+            :param behavior: type of agent to apply
         """
         super(BehaviorAgent, self).__init__(vehicle)
+        rospy.init_node('behavior_agent', anonymous=True)  # Initialize ROS node
         self._look_ahead_steps = 0
+
+        # Vehicle information
         self._speed = 0
         self._speed_limit = 0
         self._direction = None
@@ -38,17 +46,7 @@ class BehaviorAgent(BasicAgent):
         self._behavior = None
         self._sampling_resolution = 4.5
 
-        # Initialize ROS node
-        rospy.init_node('behavior_agent_node', anonymous=True)
-
-        # ROS Publishers
-        self.speed_pub = rospy.Publisher('/agent/speed', Float32, queue_size=10)
-        self.control_pub = rospy.Publisher('/carla/ego_vehicle/control_cmd', CarlaEgoVehicleControl, queue_size=10)
-
-        # ROS Subscribers
-        rospy.Subscriber('/carla/traffic_lights', CarlaTrafficLightInfo, self.traffic_light_callback)
-
-        # Behavior selection
+        # Parameters for agent behavior
         if behavior == 'cautious':
             self._behavior = Cautious()
         elif behavior == 'normal':
@@ -56,9 +54,13 @@ class BehaviorAgent(BasicAgent):
         elif behavior == 'aggressive':
             self._behavior = Aggressive()
 
+        # ROS Publisher for vehicle control commands
+        self.control_pub = rospy.Publisher('vehicle_control', carla.VehicleControl, queue_size=10)
+
     def _update_information(self):
         """
-        Updates the information about the ego vehicle.
+        This method updates the information regarding the ego
+        vehicle based on the surrounding world.
         """
         self._speed = get_speed(self._vehicle)
         self._speed_limit = self._vehicle.get_speed_limit()
@@ -68,65 +70,159 @@ class BehaviorAgent(BasicAgent):
             self._direction = RoadOption.LANEFOLLOW
 
         self._look_ahead_steps = int((self._speed_limit) / 10)
+
         self._incoming_waypoint, self._incoming_direction = self._local_planner.get_incoming_waypoint_and_direction(
             steps=self._look_ahead_steps)
         if self._incoming_direction is None:
             self._incoming_direction = RoadOption.LANEFOLLOW
 
-        # Publish vehicle speed to ROS topic
-        self.speed_pub.publish(self._speed)
-
-    def traffic_light_callback(self, msg):
-        """
-        Callback function to handle traffic light information received from ROS.
-        """
-        # Process traffic light data from ROS topic
-        if msg.state == CarlaTrafficLightInfo.RED:
-            rospy.loginfo("Red light ahead! Stopping.")
-            self.emergency_stop()
-
     def traffic_light_manager(self):
         """
-        Manage behaviors for red lights using ROS.
+        This method is in charge of behaviors for red lights.
         """
-        return False  # Traffic light data now comes from ROS
+        actor_list = self._world.get_actors()
+        lights_list = actor_list.filter("*traffic_light*")
+        affected, _ = self._affected_by_traffic_light(lights_list)
 
-    # The rest of the methods would remain similar but can now interact with ROS topics
-    # such as publishing control commands or subscribing to other sensors.
+        return affected
+
+    def _tailgating(self, waypoint, vehicle_list):
+        """
+        This method is in charge of tailgating behaviors.
+
+            :param location: current location of the agent
+            :param waypoint: current waypoint of the agent
+            :param vehicle_list: list of all the nearby vehicles
+        """
+
+        left_turn = waypoint.left_lane_marking.lane_change
+        right_turn = waypoint.right_lane_marking.lane_change
+
+        left_wpt = waypoint.get_left_lane()
+        right_wpt = waypoint.get_right_lane()
+
+        behind_vehicle_state, behind_vehicle, _ = self._vehicle_obstacle_detected(vehicle_list, max(
+            self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, low_angle_th=160)
+        if behind_vehicle_state and self._speed < get_speed(behind_vehicle):
+            if (right_turn == carla.LaneChange.Right or right_turn ==
+                    carla.LaneChange.Both) and waypoint.lane_id * right_wpt.lane_id > 0 and right_wpt.lane_type == carla.LaneType.Driving:
+                new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max(
+                    self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=1)
+                if not new_vehicle_state:
+                    print("Tailgating, moving to the right!")
+                    end_waypoint = self._local_planner.target_waypoint
+                    self._behavior.tailgate_counter = 200
+                    self.set_destination(end_waypoint.transform.location,
+                                         right_wpt.transform.location)
+            elif left_turn == carla.LaneChange.Left and waypoint.lane_id * left_wpt.lane_id > 0 and left_wpt.lane_type == carla.LaneType.Driving:
+                new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max(
+                    self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=-1)
+                if not new_vehicle_state:
+                    print("Tailgating, moving to the left!")
+                    end_waypoint = self._local_planner.target_waypoint
+                    self._behavior.tailgate_counter = 200
+                    self.set_destination(end_waypoint.transform.location,
+                                         left_wpt.transform.location)
+
+    def collision_and_car_avoid_manager(self, waypoint):
+        """
+        This module is in charge of warning in case of a collision
+        and managing possible tailgating chances.
+
+            :param location: current location of the agent
+            :param waypoint: current waypoint of the agent
+            :return vehicle_state: True if there is a vehicle nearby, False if not
+            :return vehicle: nearby vehicle
+            :return distance: distance to nearby vehicle
+        """
+
+        vehicle_list = self._world.get_actors().filter("*vehicle*")
+        def dist(v): return v.get_location().distance(waypoint.transform.location)
+        vehicle_list = [v for v in vehicle_list if dist(v) < 45 and v.id != self._vehicle.id]
+
+        if self._direction == RoadOption.CHANGELANELEFT:
+            vehicle_state, vehicle, distance = self._vehicle_obstacle_detected(
+                vehicle_list, max(
+                    self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=-1)
+        elif self._direction == RoadOption.CHANGELANERIGHT:
+            vehicle_state, vehicle, distance = self._vehicle_obstacle_detected(
+                vehicle_list, max(
+                    self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=1)
+        else:
+            vehicle_state, vehicle, distance = self._vehicle_obstacle_detected(
+                vehicle_list, max(
+                    self._behavior.min_proximity_threshold, self._speed_limit / 3), up_angle_th=30)
+
+            # Check for tailgating
+            if not vehicle_state and self._direction == RoadOption.LANEFOLLOW \
+                    and not waypoint.is_junction and self._speed > 10 \
+                    and self._behavior.tailgate_counter == 0:
+                self._tailgating(waypoint, vehicle_list)
+
+        return vehicle_state, vehicle, distance
+
+    def pedestrian_avoid_manager(self, waypoint):
+        """
+        This module is in charge of warning in case of a collision
+        with any pedestrian.
+
+            :param location: current location of the agent
+            :param waypoint: current waypoint of the agent
+            :return vehicle_state: True if there is a walker nearby, False if not
+            :return vehicle: nearby walker
+            :return distance: distance to nearby walker
+        """
+
+        walker_list = self._world.get_actors().filter("*walker.pedestrian*")
+        def dist(w): return w.get_location().distance(waypoint.transform.location)
+        walker_list = [w for w in walker_list if dist(w) < 10]
+
+        if self._direction == RoadOption.CHANGELANELEFT:
+            walker_state, walker, distance = self._vehicle_obstacle_detected(walker_list, max(
+                self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=90, lane_offset=-1)
+        elif self._direction == RoadOption.CHANGELANERIGHT:
+            walker_state, walker, distance = self._vehicle_obstacle_detected(walker_list, max(
+                self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=90, lane_offset=1)
+        else:
+            walker_state, walker, distance = self._vehicle_obstacle_detected(walker_list, max(
+                self._behavior.min_proximity_threshold, self._speed_limit / 3), up_angle_th=90)
+
+        return walker_state, walker, distance
 
     def run_step(self, debug=False):
         """
-        Execute one step of navigation and publish control commands via ROS.
+        This method executes the control for the agent based on the current world state.
+
+            :param debug: boolean flag to enable debug information
         """
         self._update_information()
 
-        control = self._local_planner.run_step(debug=debug)
+        # Manage traffic lights
+        if self.traffic_light_manager():
+            print("Stopping for traffic light!")
+            control = carla.VehicleControl(throttle=0, brake=1)
+        else:
+            control = self._local_planner.run_step()
 
-        # Create a ROS message for the vehicle control
-        control_msg = CarlaEgoVehicleControl()
-        control_msg.throttle = control.throttle
-        control_msg.brake = control.brake
-        control_msg.steer = control.steer
+        # Handle collision and pedestrian avoidance
+        waypoint = self._local_planner.get_next_waypoint()
+        vehicle_state, vehicle, distance = self.collision_and_car_avoid_manager(waypoint)
+        walker_state, walker, distance = self.pedestrian_avoid_manager(waypoint)
 
-        # Publish the control message to ROS
-        self.control_pub.publish(control_msg)
+        # Adjust speed and control based on detected vehicles and pedestrians
+        if vehicle_state:
+            control.brake = 1.0
+            control.throttle = 0.0
+            print("Braking due to nearby vehicle!")
+        elif walker_state:
+            control.brake = 1.0
+            control.throttle = 0.0
+            print("Braking due to nearby pedestrian!")
+        else:
+            control.throttle = self._behavior.max_throttle
+            control.brake = 0.0
 
-        return control
-
-    def emergency_stop(self):
-        """
-        Perform an emergency stop and publish the control message via ROS.
-        """
-        control = carla.VehicleControl()
-        control.throttle = 0.0
-        control.brake = 1.0
-        control.hand_brake = True
-
-        # Create and publish the emergency stop control via ROS
-        control_msg = CarlaEgoVehicleControl()
-        control_msg.throttle = 0.0
-        control_msg.brake = 1.0
-        control_msg.steer = 0.0
-        self.control_pub.publish(control_msg)
+        # Publish vehicle control command to ROS
+        self.control_pub.publish(control)
 
         return control
